570cee2c4c649e822b15c087e27373a94941da8b
Abdhullah Ariff <abdhullahariff118@gmail.com>
2026-05-31 19:04:12 +0530

    Fixing Redirect to dashboard issue in prod fix

--- routes.ts
+++ routes.ts
@@ -6,4 +6,4 @@ export const authRoutes: string[] = ["/auth/sign-in"];
 
 export const apiAuthPrefix = "/api/auth";
 
-export const DEFAULT_LOGIN_REDIRECT = "/";
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
